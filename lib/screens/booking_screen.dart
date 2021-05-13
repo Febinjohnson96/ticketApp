6cb06d03f31419b0229e74e5c100c404dd26a7bc
Febin Johnson <febinnj1996@gmail.com>
2021-05-14 00:04:14 +0530

    Added Booking Page

---
+++ lib/screens/booking_screen.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:ticketapp/model/movies.dart';
+import 'package:ticketapp/widget/rating.dart';
+
+class BookingScreen extends StatelessWidget {
+  final Movies moviedata;
+  BookingScreen(this.moviedata);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_outlined),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: Text(moviedata.movieName),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image(
+                width: 500,
+                height: 300,
+                fit: BoxFit.cover,
+                image: NetworkImage(moviedata.imgUrl),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RatingStars(moviedata.rating),
+              SizedBox(width: 30),
+              Text(moviedata.genre.toString()),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '27',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Date',
+                      style: TextStyle(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '14:10',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Hour',
+                      style: TextStyle(
+                        color: Colors.grey[350],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Container(
+                    padding: EdgeInsets.all(5),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                'Select seat',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Seat No',
+                            style: TextStyle(
+                              color: Colors.grey[350],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              'Synopsis',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              moviedata.desc,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.grey[400],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomSheet: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Price',
+                  style: TextStyle(
+                    color: Colors.grey[350],
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$56,6',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 60,
+              width: 160,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                child: Text('Booking'),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widget/new_movies.dart
+++ lib/widget/new_movies.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticketapp/model/movies.dart';
+import 'package:ticketapp/screens/booking_screen.dart';
 import 'package:ticketapp/widget/rating.dart';
 import '../data/data.dart';
 
@@ -10,66 +11,78 @@ class NewMovies extends StatefulWidget {
 
 class _NewMoviesState extends State<NewMovies> {
   _buildnewMovies(BuildContext context, Movies movies) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image(
-                height: 200,
-                width: 250,
-                fit: BoxFit.cover,
-                image: NetworkImage(movies.imgUrl),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => BookingScreen(
+                    movies,
+                  ),
+              settings: RouteSettings(name: 'BookScreen')),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image(
+                  height: 200,
+                  width: 250,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(movies.imgUrl),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        movies.movieName,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        movies.language,
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Row(
-                    children: [
-                      RatingStars(movies.rating),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(movies.genre.toString()),
-                    ],
-                  ),
-                ],
+              SizedBox(
+                height: 10,
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          movies.movieName,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          movies.language,
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        RatingStars(movies.rating),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(movies.genre.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
